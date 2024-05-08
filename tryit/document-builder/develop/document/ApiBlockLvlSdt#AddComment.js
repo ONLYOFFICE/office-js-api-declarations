074ba729d5a7d94a0a67400266d49e482de8f744
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-05-08 14:15:26 +0400

    migrate examples from the documentation repository

---
+++ tryit/document-builder/develop/document/ApiBlockLvlSdt#AddComment.js
@@ -0,0 +1,8 @@
+builder.CreateFile("docx");
+var oDocument = Api.GetDocument();
+var oBlockLvlSdt = Api.CreateBlockLvlSdt();
+oBlockLvlSdt.GetContent().GetElement(0).AddText("This is a block text content control.");
+oDocument.AddElement(0, oBlockLvlSdt);
+oBlockLvlSdt.AddComment("comment", "John Smith", "uid-1");
+builder.SaveFile("docx", "AddComment.docx");
+builder.CloseFile();
